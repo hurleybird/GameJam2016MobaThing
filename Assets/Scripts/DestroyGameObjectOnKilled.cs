9ee102df69ade5c0049a11c31cbd3abb13af6638
GluthB <gluthb@gmail.com>
2016-01-30 15:09:15 -0700

    Creeps are now destroyed when killed

---
+++ Assets/Scripts/DestroyGameObjectOnKilled.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class DestroyGameObjectOnKilled : MonoBehaviour
+{
+    public void Start()
+    {
+        gameObject.GetComponent<Health>().OnKilled += (_) => Destroy(gameObject);
+    }
+
+}
+
