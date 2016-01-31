4c0446b9468bd6a13f90acb20a5ae9caaf8909cd
Jeff Graw <hurleybird@gmail.com>
2016-01-31 10:57:18 -0700

    Player 2 Ability 2 Creep Spawning.

---
+++ Assets/Scripts/Abilities/Player2SpawnCreep.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Player2SpawnCreep : Ability
+{
+    [SerializeField]
+    private List<Transform> Waypoints = new List<Transform>();
+
+    protected override void FireSecondPart(GameObject newObj, Team team)
+    {
+        newObj.layer = gameObject.layer;
+        newObj.GetComponent<Renderer>().material = GetComponent<Renderer>().material;
+        Creep newCreep = newObj.GetComponent<Creep>();
+        newCreep.Init(Waypoints, team);
+    }
+}
