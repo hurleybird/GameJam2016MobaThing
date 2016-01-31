070252659c9b252b7454a9566a6b599a72ec3d0f
Jeff Graw <hurleybird@gmail.com>
2016-01-31 02:21:52 -0700

    Working Towers

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,32 +6,48 @@ public class Tower : MonoBehaviour {
     [SerializeField]
     private Team _team;
     [SerializeField]
+    private float turnRate;
+    [SerializeField]
     private Ability towerAA;
+    private LayerMask mask;
 
     public Team Team { get { return _team; } }
-	
-	//void Update () {
- //       aggro = null;
- //       RaycastHit[] hits = Physics.BoxCastAll(transform.position, new Vector3(6, 6, 6), Vector3.forward, Quaternion.identity, 100f, mask.value);
 
- //       float minDistance = float.MaxValue;
+    void Start()
+    {
+        if (gameObject.layer == 8)
+            mask = 1 << 9;
+        else
+            mask = 1 << 8;
+    }
 
- //       if (hits.Length == 0)
- //       {
- //           aggro = null;
- //       }
- //       else
- //       {
- //           foreach (RaycastHit hit in hits)
- //           {
- //               float distance = Vector3.Distance(transform.position, hit.transform.position);
- //               if (distance < minDistance)
- //               {
- //                   minDistance = distance;
- //                   aggro = hit.transform;
- //               }
- //           }
- //       }
+    void Update()
+    {
+        Transform aggro = null;
+        RaycastHit[] hits = Physics.BoxCastAll(transform.position, new Vector3(20,20,20), Vector3.forward, Quaternion.identity, 100f, mask.value);
 
- //   }
+        float minDistance = float.MaxValue;
+
+        if (hits.Length != 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                float distance = Vector3.Distance(transform.position, hit.transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    aggro = hit.transform;
+                }
+            }
+        }
+
+        if (aggro == null)
+            return;
+
+        Quaternion prevRotation = transform.rotation;
+        transform.LookAt(aggro);
+        transform.rotation = Quaternion.Slerp(prevRotation, transform.rotation, Time.deltaTime * turnRate);
+        towerAA.Activate(_team);
+
+    }
 }
