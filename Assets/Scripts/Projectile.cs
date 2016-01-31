b572d9aa9d74b3718a4b784da5be8853edb06b1e
GluthB <gluthb@gmail.com>
2016-01-31 00:08:38 -0700

    Projectiles now have a settable maximum range

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,8 +7,18 @@ public class Projectile : MonoBehaviour {
     private float speed;
     [SerializeField]
     private float damage = 1;
+    [SerializeField]
+    private float range = 5.0F;
 
+    public float Range { get { return range; } }
+
+    private float remainingDistance;
     private Team team;
+
+    public void Start()
+    {
+        remainingDistance = range;
+    }
 
     public void Init (Team _team)
     {
@@ -16,8 +26,17 @@ public class Projectile : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	void FixedUpdate () 
+    {
+        float movementThisUpdate = speed * Time.deltaTime;
+        remainingDistance -= movementThisUpdate;
+        if (remainingDistance <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.Translate(Vector3.forward * movementThisUpdate);
+        
 	}
 
     void OnTriggerEnter(Collider other)
