0ea1b0d112210df5c66d90637aaa35ccd57e28e9
GluthB <gluthb@gmail.com>
2016-01-30 22:03:56 -0700

    Creeps now shoot projectiles

---
+++ Assets/Scripts/StandardCreepWeapon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardCreepWeapon : MonoBehaviour
+{
+    private const int team1Layer = 1 << 8;
+    private const int team2Layer = 1 << 9;
+
+    [SerializeField]
+    private Ability ability;
+
+    [SerializeField]
+    private Creep creepLogic;
+
+    [SerializeField]
+    private LayerMask enemyLayer;
+
+    public void Start()
+    {
+        creepLogic.OnTargetInRange += OnHaveTargetToFireAt;
+        enemyLayer = GetEnemyLayer();
+    }
+
+    private LayerMask GetEnemyLayer()
+    {
+        if (gameObject.layer == team1Layer)
+            return team2Layer;
+        return team1Layer;
+    }
+
+    public void OnHaveTargetToFireAt(Transform targetPoint)
+    {
+        if (HaveClearShotAtTarget(targetPoint))
+        {
+            FireProjectile();
+        }
+    }
+
+    private bool HaveClearShotAtTarget(Transform targetPoint)
+    {
+        return Physics.Raycast(transform.position, transform.forward, 9000F, enemyLayer);
+    }
+
+    private void FireProjectile()
+    {
+        ability.Activate(creepLogic.Team);
+    }
+}
